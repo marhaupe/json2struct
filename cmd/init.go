7d4c6b162380ae644346b261e8c8511cc4b1a366
marhaupe <marhaupe97@gmail.com>
2018-11-26 17:06:53 +0100

    small changes

--- cmd/init.go
+++ cmd/init.go
@@ -2,21 +2,26 @@ package cmd
 
 import (
 	"encoding/json"
+	"fmt"
 	"sync"
+
+	"github.com/marhaupe/json-to-struct/internal/ds"
 
 	"github.com/marhaupe/json-to-struct/internal"
 )
 
 func Start() {
+	j := `{ 
+		"Hallo": "Hey", 
+		"Schoen": true,
+		"Number": 5999
+		}`
 	c := make(chan json.Token)
+	n := make(chan ds.JSONNode)
 	var wg sync.WaitGroup
 	wg.Add(2)
-	go internal.Lex(`{ 
-		"Key": "Value", 
-		"Key": { 
-			"Key": true
-			 } 
-		}`, c, &wg)
-	go internal.Parse(c, &wg)
+	go internal.Lex(j, c, &wg)
+	go internal.Parse(n, c, &wg)
 	wg.Wait()
+	fmt.Println(<-n)
 }
