797ecaad8c48e2694d60d8d7b8f22a579224e01e
marhaupe <marhaupe97@gmail.com>
2018-11-26 18:33:00 +0100

    added test

--- internal/ds/object_test.go
+++ internal/ds/object_test.go
@@ -60,3 +60,66 @@ func TestJSONObject_String(t *testing.T) {
 		})
 	}
 }
+
+func TestJSONObjectRoot_String(t *testing.T) {
+	type fields struct {
+		Key      string
+		Children []JSONElement
+		Root     bool
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   string
+	}{
+		{
+			name: "Nested Object Test One",
+			fields: fields{
+				Key:  "testobject",
+				Root: true,
+				Children: []JSONElement{
+					&JSONObject{
+						Key: "nested_object",
+						Children: []JSONElement{
+							&JSONPrimitive{
+								Ptype: Int,
+								Key:   "testint",
+							},
+						},
+					},
+				},
+			},
+			want: "type JSONToStruct struct{\nNested_object struct{\nTestint int `json:\"testint\"`\n} `json:\"nested_object\"`\n}\n",
+		},
+		{
+			name: "Basic Object Test One",
+			fields: fields{
+				Key:  "testobject",
+				Root: true,
+				Children: []JSONElement{
+					&JSONPrimitive{
+						Ptype: Int,
+						Key:   "testint",
+					},
+					&JSONPrimitive{
+						Ptype: String,
+						Key:   "teststring",
+					},
+				},
+			},
+			want: "type JSONToStruct struct{\nTestint int `json:\"testint\"`\nTeststring string `json:\"teststring\"`\n}\n",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			jp := &JSONObject{
+				Root:     tt.fields.Root,
+				Key:      tt.fields.Key,
+				Children: tt.fields.Children,
+			}
+			if got := jp.String(); got != tt.want {
+				t.Errorf("JSONObject.String() = \n%vwant\n%v", got, tt.want)
+			}
+		})
+	}
+}
