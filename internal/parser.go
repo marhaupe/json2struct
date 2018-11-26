9d443ef300f8ebf5465fdf87d15ccb94318e0233
marhaupe <marhaupe97@gmail.com>
2018-11-26 17:07:59 +0100

    recursively building up rootEl

--- internal/parser.go
+++ internal/parser.go
@@ -8,60 +8,95 @@ import (
 	"github.com/marhaupe/json-to-struct/internal/ds"
 )
 
-type Node struct {
-	done bool
-	el   ds.JSONNode
-	prev *Node
-	next *Node
-}
+// type Node struct {
+// 	done bool
+// 	el   ds.JSONNode
+// 	prev *Node
+// 	next *Node
+// }
 
 type Parser struct {
-	rootNode    *Node
-	currentNode *Node
+	// rootNode    *Node
+	// currentNode *Node
+	rootEl ds.JSONNode
+	c      chan json.Token
+	wg     *sync.WaitGroup
 }
 
 // Parse parsed JSON Tokens received from chan c
-func Parse(c chan json.Token, wg *sync.WaitGroup) {
-	p := Parser{}
-	p.parse(c, wg)
+func Parse(n chan ds.JSONNode, c chan json.Token, wg *sync.WaitGroup) {
+	p := Parser{c: c, wg: wg}
+	p.parse()
+	n <- p.rootEl
 }
 
 // This might help: https://play.golang.org/p/K0cb7hzc6P6
 // I could also recursively call parseObject and parseArray in here,
 // that might make some things easier (e.g detecting keys)
-func (p *Parser) parse(c chan json.Token, wg *sync.WaitGroup) {
-	defer wg.Done()
+func (p *Parser) parse() {
+	defer p.wg.Done()
+
+	// Consuming first Token, { or [
+	t := <-p.c
 
 	// Parsing root delim accordingly
-	rootNode := &Node{done: false}
-	t := <-c
 	switch t {
 	case json.Delim('{'):
-		rootNode.el = &ds.JSONObject{
+		p.rootEl = &ds.JSONObject{
 			Root: true,
 		}
 	case json.Delim('['):
-		rootNode.el = &ds.JSONArray{
+		p.rootEl = &ds.JSONArray{
 			Root: true,
 		}
 	}
-	p.rootNode = rootNode
 
-	var count int
-	for t := range c {
-		var key string
-		if count%2 == 0 {
-			fmt.Printf("Key: %v \n", t)
+	var key string
+	for t := range p.c {
+		if key == "" {
+			fmt.Printf("Key | Type %T | Value %v\n", t, t)
+			key = fmt.Sprint(t)
 		} else {
-			fmt.Printf("Value: %v \n", t)
+			fmt.Printf("Value | Type %T | Value %v\n", t, t)
+			switch t {
+			case json.Delim('{'):
+				p.rootEl.AddChild(p.parseObject(key))
+			case json.Delim('['):
+				p.rootEl.AddChild(p.parseArray(key))
+			case json.Delim(']'):
+				break
+			case json.Delim('}'):
+				break
+			default:
+				p.rootEl.AddChild(p.parsePrimitive(key, t))
+			}
+			key = ""
 		}
-		switch t {
-		case json.Delim('{'): //todo
-		case json.Delim('}'): //todo
-		case json.Delim('['): //todo
-		case json.Delim(']'): //todo
-		}
-
-		count++
 	}
+}
+
+func (p *Parser) parseObject(key string) *ds.JSONObject {
+	obj := &ds.JSONObject{}
+	return obj
+}
+
+func (p *Parser) parseArray(key string) *ds.JSONArray {
+	arr := &ds.JSONArray{}
+	return arr
+}
+
+func (p *Parser) parsePrimitive(key string, value json.Token) *ds.JSONPrimitive {
+	prim := &ds.JSONPrimitive{Key: key}
+	switch value.(type) {
+	case float64:
+		prim.Ptype = ds.Int
+	case string:
+		prim.Ptype = ds.String
+	case bool:
+		prim.Ptype = ds.Bool
+	default:
+		fmt.Printf("Could not determine datatype of field with key %v\n", key)
+		prim.Ptype = ds.String
+	}
+	return prim
 }
