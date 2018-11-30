56a6edfc110e6954e8ff9fd7768aa6592dbcb1dc
marhaupe <marhaupe97@gmail.com>
2018-11-30 20:24:39 +0100

    added recover func

--- cmd/generate.go
+++ cmd/generate.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"encoding/json"
 	"fmt"
+	"os"
 	"sync"
 
 	"github.com/marhaupe/json2struct/internal/ds"
@@ -17,6 +18,17 @@ func Generate(s string) string {
 	wg.Add(2)
 	go internal.Lex(s, c, &wg)
 	go internal.Parse(n, c, &wg)
+	defer func() {
+		if r := recover(); r != nil {
+			var ok bool
+			err, ok := r.(error)
+			if !ok {
+				err = fmt.Errorf("Error %v, exiting", r)
+				fmt.Println(err)
+				os.Exit(1)
+			}
+		}
+	}()
 	wg.Wait()
 
 	return fmt.Sprint(<-n)
