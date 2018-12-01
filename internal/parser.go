c0f46afa146610d0b83fe32eb78e718e01761a9d
marhaupe <marhaupe97@gmail.com>
2018-12-02 00:04:36 +0100

    refactored by abstracting some logic to enable sharing

--- internal/parser.go
+++ internal/parser.go
@@ -10,8 +10,6 @@ import (
 	"github.com/marhaupe/json2struct/internal/ds"
 )
 
-const FILLER_KEY string = "FILLER_KEY"
-
 type Parser struct {
 	rootEl ds.JSONNode
 	c      chan json.Token
@@ -49,13 +47,13 @@ func (p *Parser) parse() {
 }
 
 func (p *Parser) buildRootObject() {
-	obj := p.parseObject(FILLER_KEY)
+	obj := p.parseObject("ROOT_OBJECT")
 	obj.Root = true
 	p.rootEl = obj
 }
 
 func (p *Parser) buildRootArray() {
-	arr := p.parseArray(FILLER_KEY)
+	arr := p.parseArray("ROOT_ARRAY")
 	arr.Root = true
 	p.rootEl = arr
 }
@@ -73,15 +71,9 @@ func (p *Parser) parseObject(objKey string) *ds.JSONObject {
 				return obj
 			}
 		} else {
-			switch t {
-			case json.Delim('{'):
-				obj.AddChild(p.parseObject(key))
-			case json.Delim('['):
-				obj.AddChild(p.parseArray(key))
-			case json.Delim('}'):
-				return obj
-			default:
-				obj.AddChild(p.parsePrimitive(key, t))
+			obj, done := p.addElementToChildren(obj, key, t, json.Delim('}'))
+			if done {
+				return obj.(*ds.JSONObject)
 			}
 			key = ""
 		}
@@ -92,54 +84,73 @@ func (p *Parser) parseObject(objKey string) *ds.JSONObject {
 func (p *Parser) parseArray(arrKey string) *ds.JSONArray {
 	arr := &ds.JSONArray{Key: arrKey}
 
-	// Contents of array do not need a key; filling value with filler key
-	var key string
 	for t := range p.c {
-		switch t {
-		case json.Delim('{'):
-			key = "object_in_array"
-			arr.AddChild(p.parseObject(key))
-		case json.Delim('['):
-			key = "array_in_array"
-			arr.AddChild(p.parseArray(key))
-		case json.Delim(']'):
-			return arr
-		default:
-			switch t.(type) {
-			case string:
-				key = "string_in_array"
-			case bool:
-				key = "bool_in_array"
-			case float64:
-				if t == math.Trunc(t.(float64)) {
-					key = "int_in_array"
-				} else {
-					key = "float_in_array"
-				}
-			}
-			arr.AddChild(p.parsePrimitive(key, t))
-
+		key := generateArrayKeyForToken(t)
+		arr, done := p.addElementToChildren(arr, key, t, json.Delim(']'))
+		if done {
+			return arr.(*ds.JSONArray)
 		}
 	}
 	return arr
 }
 
+func (p *Parser) addElementToChildren(node ds.JSONNode, key string, t json.Token, closing json.Delim) (ds.JSONNode, bool) {
+	switch t {
+	case closing:
+		return node, true
+	case json.Delim('{'):
+		node.AddChild(p.parseObject(key))
+	case json.Delim('['):
+		node.AddChild(p.parseArray(key))
+	default:
+		node.AddChild(p.parsePrimitive(key, t))
+	}
+	return node, false
+}
+
+func generateArrayKeyForToken(t json.Token) string {
+	switch t {
+	// These delims will get detected as keys
+	case json.Delim('}'), json.Delim(']'):
+		return "closing_delim"
+	case json.Delim('{'):
+		return "object_in_array"
+	case json.Delim('['):
+		return "array_in_array"
+	default:
+		switch detectPrimitiveType(t) {
+		case ds.String:
+			return "string_in_array"
+		case ds.Bool:
+			return "bool_in_array"
+		case ds.Float:
+			return "float_in_array"
+		case ds.Int:
+			return "int_in_array"
+		}
+	}
+	panic(fmt.Sprintf("Error generating key for token %v", t))
+}
+
 func (p *Parser) parsePrimitive(key string, value json.Token) *ds.JSONPrimitive {
 	prim := &ds.JSONPrimitive{Key: key}
-	switch value.(type) {
-	case float64:
-		if value == math.Trunc(value.(float64)) {
-			prim.Ptype = ds.Int
-		} else {
-			prim.Ptype = ds.Float
-		}
-	case string:
-		prim.Ptype = ds.String
-	case bool:
-		prim.Ptype = ds.Bool
-	default:
-		fmt.Printf("Could not determine datatype of field with key %v and value %v\n", key, value)
-		prim.Ptype = ds.String
-	}
+	prim.Ptype = detectPrimitiveType(value)
 	return prim
+}
+
+func detectPrimitiveType(t json.Token) ds.PrimitiveType {
+	switch t.(type) {
+	case float64:
+		if t == math.Trunc(t.(float64)) {
+			return ds.Int
+		}
+		return ds.Float
+	case string:
+		return ds.String
+	case bool:
+		return ds.Bool
+	default:
+		fmt.Printf("Could not determine datatype of field with value %v\n", t)
+		return ds.String
+	}
 }
