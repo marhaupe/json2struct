bcd14b4a0409fbf0b8204055ba832413725f2dfb
marhaupe <marhaupe97@gmail.com>
2018-12-02 14:01:29 +0100

    commented out unused function

--- internal/ds/utils.go
+++ internal/ds/utils.go
@@ -1,14 +1,13 @@
 package ds
 
 import (
-	"regexp"
 	"strings"
 )
 
-func appendOmitEmptyToRootElement(s string) string {
-	re := regexp.MustCompile("(?s)`json:\"(.*)\"`\n$")
-	return re.ReplaceAllString(s, "`json:\"$1,omitempty\"`\n")
-}
+// func appendOmitEmptyToRootElement(s string) string {
+// 	re := regexp.MustCompile("(?s)`json:\"(.*)\"`\n$")
+// 	return re.ReplaceAllString(s, "`json:\"$1,omitempty\"`\n")
+// }
 
 func capitalizeKey(k string) string {
 	return strings.Title(k)
