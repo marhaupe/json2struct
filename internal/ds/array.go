dc2a4314bf3bfa680cfb757ed738cd5d888893eb
marhaupe <marhaupe97@gmail.com>
2018-12-20 00:35:14 +0100

    renamed jp to arr

--- internal/ds/array.go
+++ internal/ds/array.go
@@ -6,13 +6,13 @@ import (
 	"strings"
 )
 
-func (jp *JSONArray) AddChild(c JSONElement) {
-	if jp.Keys == nil {
-		jp.Keys = make(map[string]bool)
+func (arr *JSONArray) AddChild(c JSONElement) {
+	if arr.Keys == nil {
+		arr.Keys = make(map[string]bool)
 	}
 	ckey := c.GetKey()
-	if jp.Keys[ckey] {
-		for _, child := range jp.Children {
+	if arr.Keys[ckey] {
+		for _, child := range arr.Children {
 			if child.Datatype() == "object" && child.GetKey() == ckey {
 				err := mergeObjects(c, child)
 				if err != nil {
@@ -21,90 +21,77 @@ func (jp *JSONArray) AddChild(c JSONElement) {
 			}
 		}
 	} else {
-		jp.Children = append(jp.Children, c)
-		jp.Keys[ckey] = true
+		arr.Children = append(arr.Children, c)
+		arr.Keys[ckey] = true
 	}
 }
 
-func mergeObjects(source JSONElement, target JSONElement) error {
-	objToAdd, ok := source.(*JSONObject)
-	if !ok {
-		return errors.New("Error parsing JSONElement to *JSONObject")
-	}
-	childObj, ok := target.(*JSONObject)
-	for _, child := range objToAdd.Children {
-		childObj.AddChild(child)
-	}
-	return nil
+func (arr *JSONArray) GetKey() string {
+	return arr.Key
 }
 
-func (jp *JSONArray) GetKey() string {
-	return jp.Key
-}
-
-func (jp *JSONArray) Datatype() string {
+func (arr *JSONArray) Datatype() string {
 	return "array"
 }
 
-func (jp *JSONArray) String() string {
-	foundChildrenTypes := listChildrenTypes(jp.Children)
+func (arr *JSONArray) String() string {
+	foundChildrenTypes := listChildrenTypes(arr.Children)
 	var toString string
 	if len(foundChildrenTypes) == 1 {
 		dataType := foundChildrenTypes[0]
 		switch dataType {
 		case "string", "int", "bool", "float64", "interface{}":
-			toString = jp.stringPrimitive(dataType)
+			toString = arr.stringPrimitive(dataType)
 		case "object":
-			toString = jp.stringObject()
+			toString = arr.stringObject()
 		case "array":
-			toString = jp.stringArray()
+			toString = arr.stringArray()
 		default:
 			panic("Error stringifying array")
 		}
 	} else {
-		toString = jp.stringMultipleTypes()
+		toString = arr.stringMultipleTypes()
 	}
 	return toString
 }
 
-func (jp *JSONArray) stringObject() string {
+func (arr *JSONArray) stringObject() string {
 	var b strings.Builder
-	for _, child := range jp.Children {
+	for _, child := range arr.Children {
 		childObject, ok := child.(*JSONObject)
 		if !ok {
 			panic(fmt.Sprintf("Error stringifying object %v", child.GetKey()))
 		}
 		for _, grandchild := range childObject.Children {
 			grandchildString := grandchild.String()
-			// grandchildString = appendOmitEmptyToRootElement(grandchildString)
 			fmt.Fprintf(&b, grandchildString)
 		}
 	}
-	if jp.Root {
+	if arr.Root {
 		return fmt.Sprintf("type JSONToStruct []struct{\n%s}\n", b.String())
 	}
-	return fmt.Sprintf("%s []struct{\n%s} `json:\"%s\"`\n", strings.Title(jp.Key), b.String(), jp.Key)
+	return fmt.Sprintf("%s []struct{\n%s} `json:\"%s\"`\n", strings.Title(arr.Key), b.String(), arr.Key)
 }
 
-func (jp *JSONArray) stringArray() string {
-	if jp.Root {
+func (arr *JSONArray) stringArray() string {
+	if arr.Root {
 		return "type JSONToStruct [][]interface{}"
 	}
-	return fmt.Sprintf("%s [][]interface{} `json:\"%s\"`\n", strings.Title(jp.Key), jp.Key)
+	return fmt.Sprintf("%s [][]interface{} `json:\"%s\"`\n", strings.Title(arr.Key), arr.Key)
 }
 
-func (jp *JSONArray) stringPrimitive(dataType string) string {
-	if jp.Root {
+func (arr *JSONArray) stringPrimitive(dataType string) string {
+	if arr.Root {
 		return fmt.Sprintf("type JSONToStruct []%s", dataType)
 	}
-	return fmt.Sprintf("%s []%s `json:\"%s\"`\n", strings.Title(jp.Key), dataType, jp.Key)
+	return fmt.Sprintf("%s []%s `json:\"%s\"`\n", strings.Title(arr.Key), dataType, arr.Key)
 }
 
-func (jp *JSONArray) stringMultipleTypes() string {
-	if jp.Root {
+func (arr *JSONArray) stringMultipleTypes() string {
+	if arr.Root {
 		return "type JSONToStruct []interface{}"
 	}
-	return fmt.Sprintf("%s []interface{} `json:\"%s\"`\n", strings.Title(jp.Key), jp.Key)
+	return fmt.Sprintf("%s []interface{} `json:\"%s\"`\n", strings.Title(arr.Key), arr.Key)
 }
 
 func listChildrenTypes(c []JSONElement) []string {
@@ -117,4 +104,16 @@ func listChildrenTypes(c []JSONElement) []string {
 		foundChildren = append(foundChildren, k)
 	}
 	return foundChildren
+}
+
+func mergeObjects(source JSONElement, target JSONElement) error {
+	objToAdd, ok := source.(*JSONObject)
+	if !ok {
+		return errors.New("Error parsing JSONElement to *JSONObject")
+	}
+	childObj, ok := target.(*JSONObject)
+	for _, child := range objToAdd.Children {
+		childObj.AddChild(child)
+	}
+	return nil
 }
