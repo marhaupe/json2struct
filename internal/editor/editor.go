3c397953abbd6675ee604706b28a6b84f1ddab75
marhaupe <marhaupe97@gmail.com>
2019-01-21 15:44:38 +0100

    refactored to make it more testable; added tests

--- internal/editor/editor.go
+++ internal/editor/editor.go
@@ -10,25 +10,24 @@ import (
 // and consume that file and it's contents
 type Editor struct {
 	err  error
+	cmd  *exec.Cmd
 	file *os.File
 }
 
 // New initializes an Editor instance ready to be used
 func New() *Editor {
 	e := &Editor{}
-	file, err := os.OpenFile("json2struct.temp", os.O_RDWR|os.O_CREATE, 0666)
-	e.file = file
-	e.err = err
+	e.file, e.err = os.OpenFile("json2struct.temp", os.O_RDWR|os.O_CREATE, 0666)
+	e.cmd = exec.Command("vim", e.file.Name())
+	e.cmd.Stdin = os.Stdin
+	e.cmd.Stdout = os.Stdout
+	e.cmd.Stderr = os.Stderr
 	return e
 }
 
 // Display spawns a new Vim process and pipes stdin, stdout and stderr to it
 func (e *Editor) Display() {
-	cmd := exec.Command("vim", e.file.Name())
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-	cmd.Run()
+	e.cmd.Run()
 }
 
 // Consume consumes the content in the created file and closes and deletes it
--- internal/editor/editor_test.go
+++ internal/editor/editor_test.go
@@ -2,6 +2,8 @@ package editor
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 	"testing"
 )
 
@@ -26,4 +28,26 @@ func TestConsumeWithErrors(t *testing.T) {
 	if err.Error() != fmt.Errorf("Testerror").Error() {
 		t.Error("Error has not been set properly")
 	}
+}
+
+func fakeExecCommand(command string, args ...string) *exec.Cmd {
+	cs := []string{"-test.run=TestHelperProcess", "--", command}
+	cs = append(cs, args...)
+	cmd := exec.Command(os.Args[0], cs...)
+	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+	return cmd
+}
+
+func TestNew(t *testing.T) {
+	editor := New()
+	if len(editor.cmd.Args) != 2 || editor.cmd.Args[1] != editor.file.Name() {
+		t.Errorf("Cmd invoked with wrong args %v", editor.cmd.Args)
+	}
+
+	if editor.cmd.Stdout != os.Stdout ||
+		editor.cmd.Stdin != os.Stdin ||
+		editor.cmd.Stderr != os.Stderr {
+		t.Error("Cmd pipes have been set wrong")
+	}
+	editor.Consume()
 }
