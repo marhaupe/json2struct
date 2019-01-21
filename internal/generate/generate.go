4ba008ca144dc4e1b5ac3ad2e23abc89f5ac05d7
marhaupe <marhaupe97@gmail.com>
2019-01-21 14:00:21 +0100

    work in progress

--- internal/generate/generate.go
+++ internal/generate/generate.go
@@ -3,22 +3,19 @@ package internal
 import (
 	"fmt"
 	"sync"
-
-	"github.com/marhaupe/json2struct/internal/lex"
-	"github.com/marhaupe/json2struct/internal/parse"
 )
 
 // Generate takes a JSON string s and lexes and parses that string to JSONElements.
 // The resulting string is generated code for a golang struct that can be used to
 // marshal and unmarshal JSON
 func Generate(s string) (string, error) {
-	lr := make(chan lex.Result)
-	pr := make(chan parse.Result)
+	lr := make(chan LexResult)
+	pr := make(chan ParseResult)
 	var wg sync.WaitGroup
 	wg.Add(2)
 
-	go lex.Lex(s, lr, &wg)
-	go parse.Parse(pr, lr, &wg)
+	go Lex(s, lr, &wg)
+	go Parse(pr, lr, &wg)
 	wg.Wait()
 	res := <-pr
 	if res.Error != nil {
