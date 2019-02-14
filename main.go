df7b820ca1594b094ca0008b88e761c932698d5a
marhaupe <marhaupe97@gmail.com>
2019-02-14 21:11:40 +0100

    added comments

--- main.go
+++ main.go
@@ -1,3 +1,6 @@
+// json2struct is a simple cli utility that can be used to parse JSON into Go structs.
+// Behind the scenes, the tool builds an ast of the JSON and generates the
+// resulting Go code accordingly.
 package main
 
 import (
