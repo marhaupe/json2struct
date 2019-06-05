5f9d4bc36631ee116411f31e1385715c7571d9e7
marhaupe <marhaupe97@gmail.com>
2019-06-05 12:05:51 +0200

    Delete existing parse and lex logic

--- internal/generate/generate.go
+++ internal/generate/generate.go
@@ -2,32 +2,14 @@
 package generate
 
 import (
-	"fmt"
 	"go/format"
-	"sync"
-
-	"github.com/marhaupe/json2struct/internal/lex"
-	"github.com/marhaupe/json2struct/internal/parse"
 )
 
 // Generate takes a JSON string s and parses it accordingly
 // The resulting string is generated code for a Go struct that e.g. can be used to
 // marshal and unmarshal JSON
 func Generate(s string) (string, error) {
-	lr := make(chan lex.Result)
-	pr := make(chan parse.Result)
-	var wg sync.WaitGroup
-	wg.Add(2)
-
-	go lex.Lex(s, lr, &wg)
-	go parse.Parse(pr, lr, &wg)
-	wg.Wait()
-	res := <-pr
-	if res.Error != nil {
-		return "", res.Error
-	}
-
-	return fmt.Sprint(res.Node), nil
+	return "", nil
 }
 
 func GenerateWithFormatting(s string) (string, error) {
--- internal/parse/parser.go
+++ internal/parse/parser.go
@@ -1,142 +1,25 @@
-// Package parse provides fields to enable the parsing of a JSON, meant to be used in
-// conjunction with package lex
 package parse
 
-import (
-	"encoding/json"
-	"fmt"
-	"math"
-	"sync"
+type Node interface {
+	Type() NodeType
+	String() string
+	Copy() Node
+}
 
-	"github.com/marhaupe/json2struct/internal/ast"
-	"github.com/marhaupe/json2struct/internal/lex"
+type NodeType int
+
+func (t NodeType) Type() NodeType {
+	return t
+}
+
+const (
+	NodeIdent NodeType = iota
+	NodeString
+	NodeNumber
+	NodeCall
+	NodeVector
 )
 
-// Parser contains fields to enable parsing of JSON tokens to an ast
-type Parser struct {
-	rootEl ast.Node
-	lexRes chan lex.Result
-	wg     *sync.WaitGroup
-}
-
-// Result is the result of a the parsing of JSON. It either contains Node, representing
-// the root JSON node and thus the root of the ast, or an error
-type Result struct {
-	Node  ast.Node
-	Error error
-}
-
-// Parse parses JSON tokens received from lexRes and writes either the resulting error
-// or the parsed tree to parseRes
-func Parse(parseRes chan Result, lexRes chan lex.Result, wg *sync.WaitGroup) {
-	defer func() {
-		if r := recover(); r != nil {
-			err := fmt.Errorf("%v", r)
-			parseRes <- Result{Node: nil, Error: err}
-			close(parseRes)
-		}
-	}()
-
-	p := Parser{lexRes: lexRes, wg: wg}
-	p.parse()
-	parseRes <- Result{Node: p.rootEl, Error: nil}
-	close(parseRes)
-}
-
-func (p *Parser) parse() {
-	defer p.wg.Done()
-
-	// Consuming first Token, { or [
-	r := <-p.lexRes
-	if r.Error != nil {
-		panic(r.Error)
-	}
-	// Parsing root delim accordingly
-	switch r.Token {
-	case json.Delim('{'):
-		p.rootEl = p.parseObject("")
-	case json.Delim('['):
-		p.rootEl = p.parseArray("")
-	}
-}
-
-func (p *Parser) parseObject(objKey string) *ast.JSONObject {
-	obj := &ast.JSONObject{Key: objKey}
-	var key string
-	for r := range p.lexRes {
-		if r.Error != nil {
-			panic(r.Error)
-		}
-		if key == "" {
-			key = fmt.Sprint(r.Token)
-
-			// By making every second token the key, } and ] will falsely be recognized
-			// as keys instead of values
-			if key == "}" || key == "]" {
-				return obj
-			}
-		} else {
-			done := p.buildUpElement(obj, key, r.Token)
-			if done {
-				return obj
-			}
-			key = ""
-		}
-	}
-	return obj
-}
-
-func (p *Parser) parseArray(arrKey string) *ast.JSONArray {
-	arr := &ast.JSONArray{Key: arrKey}
-
-	for t := range p.lexRes {
-		if t.Error != nil {
-			panic(t.Error)
-		}
-		key := ""
-		done := p.buildUpElement(arr, key, t.Token)
-		if done {
-			return arr
-		}
-	}
-	return arr
-}
-
-func (p *Parser) buildUpElement(node ast.Node, key string, t json.Token) (done bool) {
-	switch t {
-	case json.Delim('}'), json.Delim(']'):
-		return true
-	case json.Delim('{'):
-		node.AddChild(p.parseObject(key))
-	case json.Delim('['):
-		node.AddChild(p.parseArray(key))
-	default:
-		node.AddChild(p.parsePrimitive(key, t))
-	}
-	return false
-}
-
-func (p *Parser) parsePrimitive(key string, value json.Token) *ast.JSONPrimitive {
-	prim := &ast.JSONPrimitive{Key: key}
-	prim.Datatype = detectPrimitiveType(value)
-	return prim
-}
-
-func detectPrimitiveType(t json.Token) ast.Datatype {
-	switch t.(type) {
-	case float64:
-		if t == math.Trunc(t.(float64)) {
-			return ast.Int
-		}
-		return ast.Float
-	case string:
-		return ast.String
-	case bool:
-		return ast.Bool
-	case nil:
-		return ast.Null
-	default:
-		fmt.Printf("Could not determine datatype of field with value %v\n", t)
-		return ast.String
-	}
+func ParseFromString(s string) Node {
+	return nil
 }
