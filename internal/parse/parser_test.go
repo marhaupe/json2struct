77a8469fd2d662c25afc6a427498947e71057428
marhaupe <marhaupe97@gmail.com>
2019-07-29 15:50:45 +0200

    remove `equals` helper; adjust tests

--- internal/parse/parser_test.go
+++ internal/parse/parser_test.go
@@ -1,6 +1,7 @@
 package parse
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -18,57 +19,10 @@ func mkArrayNode(children []Node) *ArrayNode {
 	}
 }
 
-func mkPrim(typ NodeType) *PrimitiveNode {
+func mkPrim(typ NodeType, value string) *PrimitiveNode {
 	return &PrimitiveNode{
 		NodeType: typ,
-	}
-}
-
-func equal(t1, t2 Node) bool {
-	if t1.Type() != t2.Type() {
-		return false
-	}
-
-	switch t1.Type() {
-	case NodeTypeArray:
-		castedt1 := t1.(*ArrayNode)
-		castedt2 := t2.(*ArrayNode)
-
-		if len(castedt1.children) != len(castedt2.children) {
-			return false
-		}
-
-		for i := 0; i < len(castedt1.children); i++ {
-
-			if !equal(castedt1.children[i], castedt2.children[i]) {
-				return false
-			}
-		}
-		return true
-
-	case NodeTypeObject:
-		castedt1 := t1.(*ObjectNode)
-		castedt2 := t2.(*ObjectNode)
-
-		if len(castedt1.children) != len(castedt2.children) {
-			return false
-		}
-
-		for key := range castedt1.children {
-			if len(castedt1.children[key]) != len(castedt2.children[key]) {
-				return false
-			}
-
-			for index := range castedt1.children[key] {
-				if !equal(castedt2.children[key][index], castedt2.children[key][index]) {
-					return false
-				}
-			}
-		}
-		return true
-
-	default:
-		return true
+		value:    value,
 	}
 }
 
@@ -99,8 +53,6 @@ func TestParseFromString(t *testing.T) {
 		{
 			name: "Object with primitives",
 			args: args{
-				// TODO: This actually does not work the way I want it to work.
-				// Ideally, the generated tree contains the value aswell.
 				json: `{
 					"teststring": "hi",
 					"testbool": true,
@@ -109,9 +61,9 @@ func TestParseFromString(t *testing.T) {
 			},
 			want: mkObjectNode(
 				map[string][]Node{
-					"teststring": []Node{PrimitiveNode{NodeType: NodeTypeString}},
-					"testbool":   []Node{PrimitiveNode{NodeType: NodeTypeBool}},
-					"testnumber": []Node{PrimitiveNode{NodeType: NodeTypeNumber}},
+					"teststring": []Node{mkPrim(NodeTypeString, "hi")},
+					"testbool":   []Node{mkPrim(NodeTypeBool, "true")},
+					"testnumber": []Node{mkPrim(NodeTypeNumber, "5.4")},
 				},
 			)},
 		{
@@ -121,15 +73,15 @@ func TestParseFromString(t *testing.T) {
 			},
 			want: mkArrayNode(
 				[]Node{
-					PrimitiveNode{NodeType: NodeTypeString},
-					PrimitiveNode{NodeType: NodeTypeString},
-					PrimitiveNode{NodeType: NodeTypeString},
+					mkPrim(NodeTypeString, "test"),
+					mkPrim(NodeTypeString, "1234"),
+					mkPrim(NodeTypeString, "true"),
 				},
 			)},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := ParseFromString(tt.args.name, tt.args.json); !equal(got, tt.want) {
+			if got := ParseFromString(tt.args.name, tt.args.json); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("ParseFromString(): \ngot:\n %#v \nwant:\n %#v", got, tt.want)
 			}
 		})
